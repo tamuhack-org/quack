ec56ee2ca88bf53570c9e9c8669b646788092175
Jay Khatri <khatri.florida@gmail.com>
2018-08-08 11:42:09 -0700

    added new link finder app

--- main.go
+++ main.go
@@ -1,32 +1,58 @@
-// This is the name of our package
-// Everything with this package name can see everything
-// else inside the same package, regardless of the file they are in
 package main
 
-// These are the libraries we are going to use
-// Both "fmt" and "net" are part of the Go standard library
 import (
-	// "fmt" has methods for formatted I/O operations (like printing to the console)
 	"fmt"
-	// The "net/http" library has methods to implement HTTP clients and servers
+	"log"
 	"net/http"
+
+	"golang.org/x/net/html"
 )
 
 func main() {
-	// The "HandleFunc" method accepts a path and a function as arguments
-	// (Yes, we can pass functions as arguments, and even trat them like variables in Go)
-	// However, the handler function has to have the appropriate signature (as described by the "handler" function below)
 	http.HandleFunc("/", handler)
-
-	// After defining our server, we finally "listen and serve" on port 8080
-	// The second argument is the handler, which we will come to later on, but for now it is left as nil,
-	// and the handler defined above (in "HandleFunc") is used
-	http.ListenAndServe(":8080", nil)
+	log.Fatal(http.ListenAndServe("0.0.0.0:8000", nil))
 }
 
-// "handler" is our handler function. It has to follow the function signature of a ResponseWriter and Request type
-// as the arguments.
 func handler(w http.ResponseWriter, r *http.Request) {
-	// For this case, we will always pipe "Hello World" into the response writer
-	fmt.Fprintf(w, "Hello World!")
+	url := r.URL.Query().Get("q")
+	fmt.Fprintf(w, "Page = %q\n", url)
+	if len(url) == 0 {
+		return
+	}
+	page, err := parse("https://" + url)
+	if err != nil {
+		fmt.Printf("Error getting page %s %s\n", url, err)
+		return
+	}
+	links := pageLinks(nil, page)
+	for _, link := range links {
+		fmt.Fprintf(w, "Link = %q\n", link)
+	}
+}
+
+func parse(url string) (*html.Node, error) {
+	fmt.Println(url)
+	r, err := http.Get(url)
+	if err != nil {
+		return nil, fmt.Errorf("Cannot get page")
+	}
+	b, err := html.Parse(r.Body)
+	if err != nil {
+		return nil, fmt.Errorf("Cannot parse page")
+	}
+	return b, err
+}
+
+func pageLinks(links []string, n *html.Node) []string {
+	if n.Type == html.ElementNode && n.Data == "a" {
+		for _, a := range n.Attr {
+			if a.Key == "href" {
+				links = append(links, a.Val)
+			}
+		}
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		links = pageLinks(links, c)
+	}
+	return links
 }
