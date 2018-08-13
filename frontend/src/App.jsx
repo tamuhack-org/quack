f3b4a1245b48a4bca02d8919d3d8ba3ca73cd8e7
Jay Khatri <khatri.florida@gmail.com>
2018-08-12 17:33:22 -0700

    dev server is running fine

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,41 +1,32 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+
+const API_URL = process.env.API_URL;
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      response: 'not loaded',
+      response: "not loaded"
     };
   }
 
   componentDidMount() {
-    console.log('hello chicken nugget')
-    fetch('https://quack.tamuhack.org/test')
+    const request_url = `${API_URL}` + "/test";
+    console.log(request_url);
+    fetch(request_url)
       .then(response => response.json())
-      .then(this.setState({ response: 'I received a message' }))
+      .then(this.setState({ response: "I received a message" }))
       .catch(err => console.log(err));
   }
-
-  // callApi() {
-  //   fetch("api/hello
-  // }
-  // callApi = async () => {
-  //   const response = await fetch("/api/hello");
-  //   const body = await response.json();
-  //
-  //   if (response.status !== 200) throw Error(body.message);
-  //
-  //   return body;
-  // };
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">{this.state.response}</p>
-      </div>
+      <header className="App-header">
+        <h1 className="App-title">Welcome to tamuhack's new registration thingy</h1>
+      </header>
+      <p className="App-intro">{this.state.response}</p>
+    </div>
     );
   }
 }
