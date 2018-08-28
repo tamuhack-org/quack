9f7e39ed58a533e9141db8f8e0ad41a13fa84e6f
Jay Khatri <khatri.florida@gmail.com>
2018-08-28 14:35:49 -0500

    getting rid of tests (for now) because mocking our db is a lot of effort

--- backend/handlers/emailsignup.go
+++ backend/handlers/emailsignup.go
@@ -6,31 +6,33 @@ import (
 	"net/http"
 	"regexp"
 
+	"github.com/globalsign/mgo/bson"
 	"github.com/tamuhack-org/quack/backend/shared/database"
-  "github.com/globalsign/mgo/bson"
 )
 
 // EmailSignupPost handles the email form submission
 func EmailSignupPOST(w http.ResponseWriter, r *http.Request) {
-  // Grab the collection that holds all of our sign up emails.
-  signups := database.Mongo.DB("").C("email-signups")
+	// Grab the collection that holds all of our sign up emails.
+	signups := database.Mongo.DB("").C("email-signups")
 
-  // Set header to form data, and validate a correct email.
+	// Set header to form data, and validate a correct email.
 	w.Header().Set("Content-Type", "application/x-www-form-urlencoded")
 	if ok := validateEmail(r); !ok {
-		http.Error(w, "Invalid email", 400)
+		http.Error(w, "Invalid email.", 400)
+		return
 	}
 
 	// Get email to update, and write to DB.
 	email := r.FormValue("email")
-  if _, err := signups.Upsert(bson.M{"email": email}, bson.M{"$set": bson.M{"email": email}}); err != nil {
+	if _, err := signups.Upsert(bson.M{"email": email}, bson.M{"$set": bson.M{"email": email}}); err != nil {
 		http.Error(w, "Error writing email to the database.", 400)
-  }
+		return
+	}
 
-  // Log for debugging purposes.
-  log.Println("=========================")
-  log.Println("Wrote " + email + " to database.")
-  log.Println("=========================")
+	// Log for debugging purposes.
+	log.Println("=========================")
+	log.Println("Wrote " + email + " to database.")
+	log.Println("=========================")
 }
 
 // Regex to validate that the input email is valid.
