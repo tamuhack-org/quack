0a1a9359c99c3494cfd994543dfa2fd4faed5e26
Jay Khatri <khatri.florida@gmail.com>
2018-09-01 14:35:20 -0500

    writing to request body and response

--- backend/handlers/events.go
+++ backend/handlers/events.go
@@ -4,19 +4,15 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"regexp"
-
-	"github.com/globalsign/mgo/bson"
-	"github.com/tamuhack-org/quack/backend/shared/database"
 )
 
 // EmailSignupPost handles the email form submission
-func EmailSignupPOST(w http.ResponseWriter, r *http.Request) {
+func EventsGET(w http.ResponseWriter, r *http.Request) {
 	// Set header to form data, and validate a correct email.
 	w.Header().Set("Content-Type", "application/json")
 
 	log.Println("=========================")
-	log.Println("Wrote " + email + " to database.")
+	log.Println("Events handler!")
+	fmt.Fprintf(w, "Events handler!")
 	log.Println("=========================")
 }
-
