83684a0b5d1e6b5d1cebd8353baaf8cd5e4736c7
Jay Khatri <khatri.florida@gmail.com>
2018-09-01 15:12:00 -0500

    change to config to read in env vars

--- backend/shared/config/config.go
+++ backend/shared/config/config.go
@@ -2,17 +2,16 @@ package config
 
 import (
 	"errors"
+	"log"
 	"os"
 	"strconv"
-  "log"
 )
 
 type Config struct {
-	MongoUrl  string `json:"MongoUrl"`
-	MongoName string `json:"MongoName"`
-	Hostname  string `json:"Hostname"`
-	SecretKey string `json:"SecretKey"`
-	Port      int    `json:"Port"`
+	MongoUrl        string `json:"MongoUrl"`
+	Port            int    `json:"Port"`
+	EventbriteUrl   string `json:"EventbriteUrl"`
+	EventbriteToken string `json:"EventbriteToken"`
 }
 
 // Load the JSON config file.
@@ -29,12 +28,21 @@ func LoadFromEnv() (*Config, error) {
 		return nil, errors.New("No database url specified!\n")
 	}
 
+	// Eventbrite url and token is optional, but it'll be logged.
+	eventbriteUrl := os.Getenv("EVENTBRITE_URL")
+	eventbriteToken := os.Getenv("EVENTBRITE_TOKEN")
+	if len(eventbriteUrl) <= 0 || len(eventbriteToken) <= 0 {
+		log.Println("Empty eventbrite metadata specified")
+	}
+
 	log.Println("----------------------------------------------------------")
-  log.Println("Starting server on port :" + strconv.Itoa(port) + " with url " + mongoUrl)
+	log.Println("Starting server on port :" + strconv.Itoa(port) + " with url " + mongoUrl)
 	log.Println("----------------------------------------------------------")
 
 	return &Config{
-		Port:      port,
-		MongoUrl:  mongoUrl,
+		Port:            port,
+		MongoUrl:        mongoUrl,
+		EventbriteUrl:   eventbriteUrl,
+		EventbriteToken: eventbriteToken,
 	}, nil
 }
