9c8709c034d3c419edc7b186c113e087fbce2d9f
Jay Khatri <khatri.florida@gmail.com>
2018-09-01 16:13:30 -0500

    request is working, need to handle err

--- backend/handlers/events.go
+++ backend/handlers/events.go
@@ -1,7 +1,8 @@
 package handlers
 
 import (
-	"fmt"
+	// "fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 
@@ -17,11 +18,25 @@ func EventsGET(w http.ResponseWriter, r *http.Request) {
 	// Global config from env vars.
 	globalConfig := config.GlobalConfig
 
+  url := globalConfig.EventbriteUrl + "/users/me/owned_events/"
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		// TODO return a status
+	}
+
+	query := req.URL.Query()
+	query.Add("token", globalConfig.EventbriteToken)
+	req.URL.RawQuery = query.Encode()
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	body, err := ioutil.ReadAll(resp.Body)
+
 	log.Println("=========================")
 	log.Println("Events handler!")
 	log.Println(globalConfig.EventbriteToken)
 	log.Println(globalConfig.EventbriteUrl)
 	log.Println("=========================")
 
-	fmt.Fprintf(w, "Events handler!")
+	w.Write(body)
 }
